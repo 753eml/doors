9021890b75e53ea7698a297e9a17b58da765516f
753eml <112660352+753eml@users.noreply.github.com>
2024-10-05 14:33:56 +0300

    Update drs.lua

--- drs.lua
+++ drs.lua
@@ -1,7 +1,14 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/main.lua"))()
-local placeIds = {6839171747, 10549820578}
+
+local placeIds = {
+    [6839171747] = "You are in a Doors match!",
+    [10549820578] = "You are in a Super Hard Mode Doors match!",
+    [6516141723] = "You are in the Doors lobby!"
+}
+
 local thumbsUpImage = "rbxassetid://97609256286565"
 local thumbsDownImage = "rbxassetid://99911273351388"
+
 local function sendNotification(title, text, duration, image)
     game:GetService("StarterGui"):SetCore("SendNotification", {  
         Title = title;
@@ -11,8 +18,31 @@ local function sendNotification(title, text, duration, image)
     })
 end
 
-if table.find(placeIds, game.PlaceId) then
-    sendNotification("Place Check", "You are in a Doors match!", 10, thumbsUpImage)
+-- Sound ID maps for different places
+local soundIdMaps = {
+    [6839171747] = {
+        ["rbxassetid://11447013731"] = "rbxassetid://5188314808",
+        ["rbxassetid://7758469482"] = "rbxassetid://5037969255",
+        ["rbxassetid://8007673711"] = "rbxassetid://9114149321",
+        ["rbxassetid://16604121645"] = "rbxassetid://5037969255"
+    },  -- Added comma here
+    [10549820578] = {
+        ["rbxassetid://8007673711"] = "rbxassetid://9114149321",
+        ["rbxassetid://16604121645"] = "rbxassetid://5037969255",
+        ["rbxassetid://11447013731"] = "rbxassetid://5188314808",
+        ["rbxassetid://7758469482"] = "rbxassetid://5037969255"
+    },  -- Added comma here
+    [6516141723] = {
+        ["rbxassetid://9145204049"] = "rbxassetid://101837480757161"
+    }
+}
+
+local defaultVolume = 1.6  -- Set volume (0-10)
+
+if placeIds[game.PlaceId] then
+    sendNotification("Place Check", placeIds[game.PlaceId], 10, thumbsUpImage)
+
+    -- Door detection logic
     workspace.ChildAdded:Connect(function(child)      
         if child:IsA("Part") and child:FindFirstChild("ClickDetector") then      
             child.ClickDetector.MouseClick:Connect(function()            
@@ -21,27 +51,24 @@ if table.find(placeIds, game.PlaceId) then
             print("Connected new door:", child.Name)    
         end
     end)
-    local soundIdMap = {
-        ["rbxassetid://11447013731"] = "rbxassetid://5188314808",
-        ["rbxassetid://7758469482"] = "rbxassetid://5037969255",
-        ["rbxassetid://8007673711"] = "rbxassetid://9114149321",
-        ["rbxassetid://16604121645"] = "rbxassetid://5037969255",
-        ["rbxassetid://17717855685"] = "rbxassetid://5037969255",
-        ["rbxassetid://9114149321"] = "rbxassetid://8007673711"
-    }
-    local defaultVolume = 1.6  -- Set volume (0-10)
-    while true do
-        for _, v in pairs(workspace:GetDescendants()) do
-            if v:IsA("Sound") then  
-                local newSoundId = soundIdMap[v.SoundId]
-                if newSoundId then
-                    v.SoundId = newSoundId
-                    v.Volume = defaultVolume
+
+    -- Use the correct soundIdMap based on the place
+    local soundIdMap = soundIdMaps[game.PlaceId]
+
+    if soundIdMap then
+        while true do
+            for _, v in pairs(workspace:GetDescendants()) do
+                if v:IsA("Sound") then  
+                    local newSoundId = soundIdMap[v.SoundId]
+                    if newSoundId then
+                        v.SoundId = newSoundId
+                        v.Volume = defaultVolume
+                    end
                 end
             end
+            wait(1)
         end
-        wait(1)  -- Recommended: Keep the delay, but you can change it, as I lagged when I tried without it.
     end
 else
-    sendNotification("Error while loading sound modifications -", "You are not in a Doors match!", 10, thumbsDownImage) 
+    sendNotification("Error while loading sound modifications", "You are not in Doors!", 10, thumbsDownImage)
 end
