5140fb78dc7ccf416589dcbb6ddc7d2c42f04c9a
753eml <112660352+753eml@users.noreply.github.com>
2024-10-10 22:36:30 +0300

    Update drs.lua

--- drs.lua
+++ drs.lua
@@ -8,15 +8,12 @@ if getgenv().doorshacksloaded then
     })
     return
 end
-
 getgenv().doorshacksloaded = true
-
 local placeIds = {
     [6839171747] = "You are in a Doors match!",
     [10549820578] = "You are in a Super Hard Mode Doors match!",
     [6516141723] = "You are in the Doors lobby!"
 }
-
 local thumbsUpImage = "rbxassetid://97609256286565"
 local thumbsDownImage = "rbxassetid://99911273351388"
 
@@ -28,39 +25,37 @@ local function sendNotification(title, text, duration, image)
         Icon = image;
     })
 end
-
 local soundIdMaps = {
     [6839171747] = {
-        ["rbxassetid://11447013731"] = {id = "rbxassetid://5188314808", volume = 1.0},
-        ["rbxassetid://7758469482"] = {id = "rbxassetid://5037969255", volume = 1.2},
-        ["rbxassetid://8007673711"] = {id = "rbxassetid://9114149321", volume = 0.8},
-        ["rbxassetid://16604121645"] = {id = "rbxassetid://5037969255", volume = 1.1},
-        ["rbxassetid://10470707502"] = {id = "rbxassetid://101837480757161", volume = 0.9},
-        ["rbxassetid://6973423505"] = {id = "rbxassetid://6973423694", volume = 1.5},  -- New
-        ["rbxassetid://9113549320"] = {id = "rbxassetid://8248258948", volume = 1.0},
-        ["rbxassetid://10460221938"] = {id = "rbxassetid://10907273416", volume = 0.7},
-        ["rbxassetid://10472770795"] = {id = "rbxassetid://11638638410", volume = 1.4},
-        ["rbxassetid://10470715177"] = {id = "rbxassetid://5246103002", volume = 1.3},
-        ["rbxassetid://17717855685"] = {id = "rbxassetid://103523196237716", volume = 1.0},  -- New
-        ["rbxassetid://103523196237716"] = {id = "rbxassetid://5188314808", volume = 1.0}    -- New
+        ["rbxassetid://11447013731"] = {id = "rbxassetid://5188314808", volume = 1.6},
+        ["rbxassetid://7758469482"] = {id = "rbxassetid://5037969255", volume = 1.6},
+        ["rbxassetid://8007673711"] = {id = "rbxassetid://9114149321", volume = 1.6},
+        ["rbxassetid://16604121645"] = {id = "rbxassetid://5037969255", volume = 1.6},
+        ["rbxassetid://10470707502"] = {id = "rbxassetid://101837480757161", volume = 1.6},
+        ["rbxassetid://6973423505"] = {id = "rbxassetid://6973423694", volume = 1.6},
+        ["rbxassetid://9113549320"] = {id = "rbxassetid://8248258948", volume = 1.6},
+        ["rbxassetid://10460221938"] = {id = "rbxassetid://10907273416", volume = 1.6},
+        ["rbxassetid://10472770795"] = {id = "rbxassetid://11638638410", volume = 1.6},
+        ["rbxassetid://10470715177"] = {id = "rbxassetid://5246103002", volume = 1.6},
+        ["rbxassetid://17717855685"] = {id = "rbxassetid://103523196237716", volume = 1.6},
+        ["rbxassetid://103523196237716"] = {id = "rbxassetid://5188314808", volume = 1.6}
     },
     [10549820578] = {
-        ["rbxassetid://8007673711"] = {id = "rbxassetid://9114149321", volume = 0.8},
-        ["rbxassetid://16604121645"] = {id = "rbxassetid://5037969255", volume = 1.1},
-        ["rbxassetid://11447013731"] = {id = "rbxassetid://5188314808", volume = 1.0},
-        ["rbxassetid://7758469482"] = {id = "rbxassetid://5037969255", volume = 1.2},
-        ["rbxassetid://6973423505"] = {id = "rbxassetid://6973423694", volume = 1.5},  -- New
-        ["rbxassetid://10460221938"] = {id = "rbxassetid://10907273416", volume = 0.7},
-        ["rbxassetid://10472770795"] = {id = "rbxassetid://11638638410", volume = 1.4},
-        ["rbxassetid://10470715177"] = {id = "rbxassetid://5246103002", volume = 1.3},
-        ["rbxassetid://17717855685"] = {id = "rbxassetid://103523196237716", volume = 1.0},  -- New
-        ["rbxassetid://103523196237716"] = {id = "rbxassetid://5188314808", volume = 1.0}    -- New
+        ["rbxassetid://8007673711"] = {id = "rbxassetid://9114149321", volume = 1.6},
+        ["rbxassetid://16604121645"] = {id = "rbxassetid://5037969255", volume = 1.6},
+        ["rbxassetid://11447013731"] = {id = "rbxassetid://5188314808", volume = 1.6},
+        ["rbxassetid://7758469482"] = {id = "rbxassetid://5037969255", volume = 1.6},
+        ["rbxassetid://6973423505"] = {id = "rbxassetid://6973423694", volume = 1.6},
+        ["rbxassetid://10460221938"] = {id = "rbxassetid://10907273416", volume = 1.6},
+        ["rbxassetid://10472770795"] = {id = "rbxassetid://11638638410", volume = 1.6},
+        ["rbxassetid://10470715177"] = {id = "rbxassetid://5246103002", volume = 1.6},
+        ["rbxassetid://17717855685"] = {id = "rbxassetid://103523196237716", volume = 1.6},
+        ["rbxassetid://103523196237716"] = {id = "rbxassetid://5188314808", volume = 1.6}
     },
     [6516141723] = {
-        ["rbxassetid://7767565697"] = {id = "rbxassetid://11638638410", volume = 1.0}
+        -- lobby sounds didnt work
     }
 }
-
 if placeIds[game.PlaceId] then
     sendNotification("Place Check", placeIds[game.PlaceId], 10, thumbsUpImage)
     -- MSPaint
